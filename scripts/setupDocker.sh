536bd012539fb6103c4233987b8aaf09b46154e9
Daniel Barclay <barclaysd@me.com>
2020-11-20 12:41:17 +0000

    Add missing steps to setup docker

--- scripts/setupDocker.sh
+++ scripts/setupDocker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build --build-arg ACCESS_TOKEN_SECRET="$1" --build-arg REFRESH_TOKEN_SECRET="$2"
+docker build --build-arg ACCESS_TOKEN_SECRET="$1" --build-arg REFRESH_TOKEN_SECRET="$2" -t phonebook-api-test -f Dockerfile.test .
 docker tag phonebook-api-test phonebook-api-test:latest
 docker-compose up -d
